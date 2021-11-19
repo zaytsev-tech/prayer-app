0d10b287c96d2abb8b2a287f85f1f1d7d10aac5c
Aleksandr Zaytsev <aleksandrzaytsev@MacBook-Pro-User.local>
2021-11-19 14:19:37 +0600

    fix(init): update eslint config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    [
+      'module:metro-react-native-babel-preset',
+      { useTransformReactJSXExperimental: true },
+    ],
+    '@babel/preset-typescript',
+  ],
+  sourceMaps: 'inline',
+  plugins: [
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
+    [
+      '@babel/plugin-transform-react-jsx',
+      {
+        runtime: 'automatic',
+      },
+    ],
+    '@babel/proposal-object-rest-spread',
+    ['babel-plugin-styled-components'],
+    'react-native-reanimated/plugin',
+  ],
 };
